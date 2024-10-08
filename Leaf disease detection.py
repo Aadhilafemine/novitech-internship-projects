4805dedcbc1e7909936a6ab46d09d00dbf016a9c
Aadhilafemine <115147956+Aadhilafemine@users.noreply.github.com>
2024-10-08 22:54:22 +0530

    Add files via upload

---
+++ Leaf disease detection.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+from keras.models import Sequential
+#initialize nn
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+#convert pooling features space to large feature vector for fully
+#connected layer 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+
+#basic cnn
+model = Sequential()
+model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(128,128, 3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(96, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Dense(25, activation = 'softmax'))
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+
+train_datagen = ImageDataGenerator(rescale = None,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/train',
+                                                 target_size = (128, 128),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+#print(test_datagen);
+labels = (training_set.class_indices)
+print(labels)
+
+test_set = test_datagen.flow_from_directory('dataset/test',
+                                            target_size = (128, 128),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+labels2 = (test_set.class_indices)
+print(labels2)
+
+model.fit_generator(training_set,
+                         steps_per_epoch = 375,
+                         epochs = 10,
+                         validation_data = test_set,
+                         validation_steps = 125)
+
+
+# Part 3 - Making new predictions
+##
+##model_json=model.to_json()
+##with open("model1.json", "w") as json_file:
+##    json_file.write(model_json)
+### serialize weights to HDF5
+##    model.save_weights("model1.h5")
+##    print("Saved model to disk")
+
+#predict fin GUI
+
+
+
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+from keras.preprocessing import image
+from keras.models import model_from_json
+from tkinter import *
+
+
+win = tk.Tk()
+
+lbl = Label(win, text="", fg='black')
+lbl.pack()
+
+def b1_click():
+    global path2
+    try:
+        json_file = open('model1.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("model1.h5")
+
+
+        label=["Apple___Apple_scab","Apple___Black_rot","Apple___Cedar_apple_rust","Apple___Healthy",
+               "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot","Corn_(maize)___Common_rust_",
+               "Corn_(maize)___Healthy","Corn_(maize)___Northern_Leaf_Blight","Grape___Black_rot",
+               "Grape___Esca_(Black_Measles)","Grape___Healthy","Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
+               "Potato___Early_blight","Potato___Healthy","Potato___Late_blight","Tomato___Bacterial_spot",
+               "Tomato___Early_blight","Tomato___Healthy","Tomato___Late_blight","Tomato___Leaf_Mold",
+               "Tomato___Septoria_leaf_spot","Tomato___Spider_mites Two-spotted_spider_mite","Tomato___Target_Spot",
+               "Tomato___Tomato_Yellow_Leaf_Curl_Virus","Tomato___Tomato_mosaic_virus"]
+
+        path2=filedialog.askopenfilename()
+        print(path2)
+        
+
+
+        test_image = image.load_img(path2, target_size=(128,128))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = loaded_model.predict(test_image)
+        print(result)
+        label2=label[result.argmax()]
+        print(label2)
+        lbl.configure(text=label2)
+        win.mainloop()
+    except IOError:
+        pass
+
+label1=Label(win, text="GUI For Leaf Disease Detection using OpenCV",fg='blue')
+label1.pack()
+
+b1=tk.Button(win,text="browse image",width=25,height=3,fg='red',command=b1_click)
+b1.pack()
+win.geometry("550x250")
+win.title("Leaf Disease Detection using OpenCV")
+win.bind("<Return>", b1_click)
+win.mainloop()
